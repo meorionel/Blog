4b9f4c5f48b4869bd4fd2fb5929871fcd6e365cc
meorionel <therese.sjdyka@gmail.com>
2025-12-26 10:46:10 +0800

    feat: Merge code

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,6 @@
 <template>
 	<div class="py-12">
-<<<<<<< HEAD
-=======
 		<NuxtLoadingIndicator color="#ea9a97" />
->>>>>>> upstream/main
 		<AppHeader />
 		<div class="min-h-screen py-8">
 			<NuxtPage />
--- app/plugins/mediumZoom.client.js
+++ app/plugins/mediumZoom.client.js
@@ -1,33 +1,15 @@
 import { defineNuxtPlugin } from "#app";
 import mediumZoom from "medium-zoom";
 
-<<<<<<< HEAD
-export default defineNuxtPlugin((nuxtApp) => {
-	const selector = ".prose img";
-
-	let zoomInstance = mediumZoom(selector, {
-		margin: 24,
-		background: "rgba(0, 0, 0, 0.8)",
-		scrollOffset: 40,
-	});
-
-	nuxtApp.hook("page:finish", () => {
-		zoomInstance.detach().attach(selector);
-=======
 export default defineNuxtPlugin(() => {
 	const zoom = mediumZoom({
 		margin: 24,
 		background: "rgba(0,0,0,0.6)",
->>>>>>> upstream/main
 	});
 
 	return {
 		provide: {
-<<<<<<< HEAD
-			contentEnhancerZoom: zoomInstance,
-=======
 			zoom,
->>>>>>> upstream/main
 		},
 	};
 });
