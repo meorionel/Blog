674d88bf1af14675f8b2fa3ac9cce01501dbbf65
Meorion <120046369+meorionel@users.noreply.github.com>
2026-01-06 14:10:27 +0800

    Update copyright year in appFooter component

--- app/components/appFooter.vue
+++ app/components/appFooter.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="mx-auto flex w-172 items-center justify-between text-sm text-gray-500 max-md:w-screen max-md:px-4">
-		<span>&copy; 2025 PinkLine. All rights reserved.</span>
+		<span>&copy; 2025~2026 PinkLine. All rights reserved.</span>
 		<ThemeSwitch />
 	</div>
 </template>
